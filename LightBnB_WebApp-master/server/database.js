0d5c628636f97b6d7d6cd7fa322fff002b3f5bde
Sarah Moss <sarah-moss@live.com>
2022-06-18 15:27:12 +0000

    Refactor getAllProperties function to use data from the lightbnb database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,5 +1,19 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+const { Pool } = require('pg');
+
+// Using a Pool instead of Client is the preferred way to query with node-postgres. A Pool will manage multiple client connections for us which we don't really need to worry about right now. Just know that either could be used, but Pool is preferred.
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+pool.connect();
+// pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log(response)})
+
 
 /// Users
 
@@ -66,15 +80,35 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
-}
+// const getAllProperties = function(options, limit = 10) {
+//   const limitedProperties = {};
+//   for (let i = 1; i <= limit; i++) {
+//     limitedProperties[i] = properties[i];
+//   }
+//   return Promise.resolve(limitedProperties);
+// }
+
+// -------------------------------------------------------------------------------------------
+
+const getAllProperties = (options, limit = 10) => {
+
+  pool
+    .query(
+      `SELECT * properties FROM properties
+      LIMIT $1
+      `,
+      [limit])
+    .then((result) => {
+      console.log(result.rows);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
 exports.getAllProperties = getAllProperties;
 
+// -------------------------------------------------------------------------------------------
 
 /**
  * Add a property to the database
