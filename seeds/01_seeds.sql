589e05c47dc14f26305caaaf1d1af56528ec2d54
Sarah Moss <sarah-moss@live.com>
2022-06-24 20:24:37 +0000

    Remove duplicate reservations data

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,8 +1,3 @@
-INSERT INTO reservations (start_date, end_date, property_id, guest_id)
-VALUES ('2018-09-11', '2018-09-26', 1, 1),
-('2019-01-04', '2019-02-01', 2, 2),
-('2021-10-01', '2021-10-14', 3, 3);
-
 INSERT INTO users (name, email, password)
 VALUES 
 ('Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
